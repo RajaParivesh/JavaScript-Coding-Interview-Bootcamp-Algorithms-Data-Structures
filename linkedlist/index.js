5bb0addcf9d76bb5956ca80630ebdbaac2b036cd
RAJA PARIVESH <rajaparivesh@gmail.com>
2018-07-28 09:35:04 +0530

    Linklist: Insert a node at given index

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -156,6 +156,21 @@ class LinkedList {
 		previous.next = previous.next.next;
 		
 	}
+	insertAt(data,index){
+		let node = this.head;
+		if(!node || index==0){
+			node = this.insertFirst(data);
+		}
+		if(node){
+			let previous = this.getAt(index-1) || this.getLast();
+			const node = new Node(data, previous.next);
+			previous.next = node;
+
+		}
+
+		console.log(node);
+	}
+
 }
 	
 	
@@ -163,7 +178,7 @@ class LinkedList {
 	list.head = new Node(10);
 	list.insertFirst(20);
 	list.insertFirst(30);
-	// console.log(list);
+	console.log(list);
 	// console.log(list.size());
 // console.log(list.getFirst());
 // console.log(list.getLast());
@@ -173,5 +188,6 @@ class LinkedList {
 // console.log(list.removeLast());
 // list.insertLast(26);
 // list.getAt(3);
-list.removeAt(0);
+// list.removeAt(0);
+list.insertAt(32,3);
 module.exports = { Node, LinkedList };
