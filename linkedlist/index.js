5bbdb5d0ae1b63f3a1b34827ffbfb4e0895e38c4
RAJA PARIVESH <rajaparivesh@gmail.com>
2018-07-28 07:03:39 +0530

    Linklist: Remove node at given index

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -3,86 +3,86 @@
 // See 'directions' document
 
 class Node {
-	constructor(data, next = null){
-		this.data = data;
-		this.next = next;
-	}
+  constructor(data, next = null){
+      this.data = data;
+      this.next = next;
+  }
 }
 
 class LinkedList {
+  
+  constructor(){
+    this.head = null; 
+  }
+  
+  insertFirst(data){
+  	this.head = new Node(data, this.head);
+  }
+  
+  size(){
+  	let counter = 0;
+  	let node = this.head;
 
-	constructor(){
-		this.head = null;
-	}
+  	while(node){
+  		counter++;
+  		node = node.next;
+  	}
+  	return counter;
+  }
+  
+  getFirst(){
+  	return this.head;
+  }
+  
+  getLast(){
+  	let node = this.head;
+  	if(!node){
+  		return null;
+  	}
+  	while(node){
+  		if(!node.next){
+  		  	return node;
+  		}
+  	node = node.next;
+  	}
+  }
 
-	insertFirst(data){
-		this.head = new Node(data, this.head);
-	}
+  clear(){
 
-	size(){
-		let counter = 0;
-		let node = this.head;
+  	this.head = null;
+  	return "cleared";11
 
-		while(node){
-			counter++;
-			node = node.next;
-		}
-		return counter;
-	}
+  }
 
-	getFirst(){
-		return this.head;
-	}
-
-	getLast(){
-		let node = this.head;
-		if(!node){
-			return null;
-		}
-		while(node){
-			if(!node.next){
-				return node;
-			}
-			node = node.next;
-		}
-	}
-
-	clear(){
-
-		this.head = null;
-		return "cleared";11
-
-	}
-
-	removeFirst(){
-		if(!this.head){
-			return;
-		}
-		return this.head = this.head.next;
-	}
+  removeFirst(){
+  	if(!this.head){
+  		return;
+  	}
+  	return this.head = this.head.next;
+  }
 
 
-	removeLast(){
+   removeLast(){
+  	
+  	if(!this.head){
+  		return;
+  	}
 
-		if(!this.head){
-			return;
-		}
-
-		if(!this.head.next){
-			this.head = null;
-			return;
-		}
-
-		let previous = this.head;
-		let node = this.head.next;
-		while(node.next){
-			previous = node;
-			node = node.next;
-		}
-		previous.next = null;
-	}
-	// // self // working //
-	// insertLast(data){
+  	if(!this.head.next){
+  		this.head = null;
+  		return;
+  	}
+  
+  	let previous = this.head;
+  	let node = this.head.next;
+  	while(node.next){
+  		previous = node; 
+  		node = node.next;
+  	}
+  	previous.next = null;
+  }
+  	// // self // working // 
+  	// insertLast(data){
 	// 	var node = this.head;
 	// 	if(!node){
 	// 		this.head = data;
@@ -100,14 +100,14 @@ class LinkedList {
 	// 	console.log(node);
 	// }
 
-	insertLast(data){
-		const last = this.getLast();
-		if(last){
-			last.next = new Node(data);
-		}else{
-			this.head = new Node(data);
-		}
-		// console.log(this.head);
+  	insertLast(data){
+	const last = this.getLast();
+	if(last){
+		last.next = new Node(data); 
+	}else{
+		this.head = new Node(data);
+	}
+	// console.log(this.head); 
 	}
 
 	// working but takes 20ms
@@ -119,7 +119,7 @@ class LinkedList {
 	// 		console.log(node);
 	// 		return node;
 	// 	}else{
-	// 		for(let i = 1; i <= index; i++ ){
+	// 		for(let i = 1; i <= index; i++ ){		
 	// 			node = node.next;
 	// 		}
 	// 		console.log(node);
@@ -133,28 +133,45 @@ class LinkedList {
 		var node = this.head;
 		while(node){
 			if(counter == index){
-				return node;
+				return node; 
 			}
 			counter++;
 			node = node.next;
 		}
 		return null;
 	}
+	removeAt(index){
+		var node = this.head;
+		if(!node){
+			return;
+		}
+		if(index==0){
+			this.head = this.head.next;
+			return;
+		}
+		const previous = this.getAt(index - 1);
+		if(!previous || !previous.next){
+			return null;
+		}
+		previous.next = previous.next.next;
+		
+	}
 }
-
-
+	
+	
 	const list = new LinkedList();
 	list.head = new Node(10);
 	list.insertFirst(20);
 	list.insertFirst(30);
 	// console.log(list);
 	// console.log(list.size());
-	// console.log(list.getFirst());
-	// console.log(list.getLast());
-	// console.log(list.clear());
-	// console.log(list);
-	// console.log(list.removeFirst());
-	// console.log(list.removeLast());
-	// list.insertLast(26);
-	list.getAt(3);
-	module.exports = { Node, LinkedList };
+// console.log(list.getFirst());
+// console.log(list.getLast());
+// console.log(list.clear());
+// console.log(list);
+// console.log(list.removeFirst());
+// console.log(list.removeLast());
+// list.insertLast(26);
+// list.getAt(3);
+list.removeAt(0);
+module.exports = { Node, LinkedList };
