508cb3872919bb51d754e4503fc75ea788871252
RajaParivesh <rajaparivesh@gmail.com>
2018-08-18 17:02:14 +0530

    Recursion: Method 2 - Count no. of times searchString is present inside a string using recursion

--- 2.PractiseQuestions/recursion/countSubstringRecursively.js
+++ 2.PractiseQuestions/recursion/countSubstringRecursively.js
@@ -5,16 +5,37 @@
 // Output: 2
 
 
+// Method 1
+
+// function countSubstringRecursively(string, searchString){
+//     if(string.length < searchString.length){
+//         return 0;
+//     }
+//     if(string.substring(0, searchString.length) === searchString){
+//         string = string.slice(searchString.length, string.length);
+//         return countSubstringRecursively(string, searchString) + 1;
+//     }
+//     string = string.slice(1, string.length);
+//     return countSubstringRecursively(string, searchString);
+// }
+// console.log(countSubstringRecursively('abcabc', 'ab'));
+
+
+// Method 2
+
 function countSubstringRecursively(string, searchString){
     if(string.length < searchString.length){
         return 0;
     }
+    let count = 0;
     if(string.substring(0, searchString.length) === searchString){
         string = string.slice(searchString.length, string.length);
-        return countSubstringRecursively(string, searchString) + 1;
+        count = 1;
+    } else {
+        string = string.slice(1, string.length);
     }
-    string = string.slice(1, string.length);
-    return countSubstringRecursively(string, searchString);
+    
+    return count + countSubstringRecursively(string, searchString);
 }
 
 console.log(countSubstringRecursively('abcabc', 'ab'));
